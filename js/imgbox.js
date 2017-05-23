8b934a3a36abae85feba39bde1fa1d02ec6cb5e6
hujun <1450611414@qq.com>
2017-05-23 22:21:57 +0800

    Site updated: 2017-05-23 22:21:57

--- js/imgbox.js
+++ js/imgbox.js
@@ -1,3 +1,3 @@
-// build time:Tue May 23 2017 22:17:50 GMT+0800 (中国标准时间)
+// build time:Tue May 23 2017 22:21:52 GMT+0800 (中国标准时间)
 $(document).ready(function(){$("p img").each(function(){var c="<a id='yourid' href='"+this.src+"'></a>";$(this).wrapAll(c)});$("#yourid").fancybox({openEffect:"elastic",closeEffect:"elastic"})});
 //rebuild by neat 
